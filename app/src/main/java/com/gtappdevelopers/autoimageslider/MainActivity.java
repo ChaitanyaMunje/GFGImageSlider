3631e2d9c6cf89b63eb1c7c4cf04203c40dfc896
chaitanyamunje@gmail.com <Munje@1069>
2020-12-15 13:37:03 +0530

    PDF Viewer

--- app/src/main/java/com/gtappdevelopers/autoimageslider/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/autoimageslider/MainActivity.java
@@ -2,41 +2,69 @@ package com.gtappdevelopers.autoimageslider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.smarteist.autoimageslider.SliderView;
+import com.github.barteksc.pdfviewer.PDFView;
 
-import java.util.ArrayList;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-    // Urls of our images.
-    String url1="https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
-    String url2="https://qphs.fs.quoracdn.net/main-qimg-8e203d34a6a56345f86f1a92570557ba.webp";
-    String url3="https://bizzbucket.co/wp-content/uploads/2020/08/Life-in-The-Metro-Blog-Title-22.png";
+
+    //creating a variable for PDF view.
+    PDFView pdfView;
+    //url of our PDF file.
+    String pdfurl="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // we are creating array list for storing our image urls.
-        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
-        // initializing the slider view.
-        SliderView sliderView = findViewById(R.id.slider);
-        // adding the urls inside array list
-        sliderDataArrayList.add(new SliderData(url1));
-        sliderDataArrayList.add(new SliderData(url2));
-        sliderDataArrayList.add(new SliderData(url3));
-        // passing this array list inside our adapter class.
-        SliderAdapter adapter = new SliderAdapter(this, sliderDataArrayList);
-        //below method is used to set auto cycle direction in left to right direction you can change according to requirement.
-        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
-        //below method is used to setadapter to sliderview.
-        sliderView.setSliderAdapter(adapter);
-        //below method is use to set scroll time in seconds.
-        sliderView.setScrollTimeInSec(3);
-        //to set it scrollable automatically we use below method.
-        sliderView.setAutoCycle(true);
-        //to start autocycle below method is used.
-        sliderView.startAutoCycle();
+        //initializing our pdf view.
+        pdfView = findViewById(R.id.idPDFView);
+        new RetrivePDFfromUrl().execute(pdfurl);
+
     }
+
+    //create an async task class for loading pdf file from URL.
+    class RetrivePDFfromUrl extends AsyncTask<String, Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            //we are using inputstream for getting out PDF.
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                // below is the step where we are creating our connection.
+                HttpURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    //response is success.
+                    //we are getting input stream from url and storing it in our variable.
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                }
+
+            } catch (IOException e) {
+                //this is the method to handle errors.
+                e.printStackTrace();
+                return null;
+            }
+
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            // after the execution of our async task we are loading our pdf in our pdf view.
+            pdfView.fromStream(inputStream).load();
+
+        }
+    }
+
 }
